4121ccad88a59820c8da4770cf1e5e4bea519cc2
Julián Melero Hidalgo <julian@melerohidalgo.es>
2020-12-31 12:37:15 +0100

    after-each/all before-each/all

---
+++ __test__/setup.test.js
@@ -0,0 +1,15 @@
+// después de cada prueba
+
+afterEach(() => console.log('Después de cada prueba'));
+afterAll( () => console.log('Después de todas las pruebas'));
+
+//  antes de cada prueba
+
+beforeAll( () => console.log('Antes de todas las pruebas'));
+beforeEach(() => console.log('Antes de cada prueba'));
+
+describe('Preparar antes de ejecutar', () => {
+    test('Es verdadero', () => {
+        expect(true).toBeTruthy();
+    });
+});
