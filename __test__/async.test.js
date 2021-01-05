0e14bbc2f6f62291fa2ba40004f430cfe3b73086
Julián Melero Hidalgo <julian@melerohidalgo.es>
2021-01-05 14:24:26 +0100

    Async/await API error

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -13,4 +13,15 @@ describe('Probar Async / Await', () => {
             expect(data.name).toEqual('Rick Sanchez');
         });
     });
+
+    test('Realizando petición a API con error', async () => {
+        const apiError = 'http://httpstat.us/404';
+        const peticion = getDataFromAPI(apiError);
+        await  expect(peticion).rejects.toEqual(Error('Request failed with status code 404'));
+        });    
+
+    test('Resuelve un Hola', async () => {
+        await expect(Promise.resolve('Hola')).resolves.toBe('Hola');
+        await expect(Promise.reject('Error')).rejects.toBe('Error');
+        });
 });
