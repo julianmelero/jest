f5075b6b3855c08e5d65f04e32173b5e25d01912
Julián Melero Hidalgo <julian@melerohidalgo.es>
2021-01-05 13:04:28 +0100

    Resolve Reject

--- __test__/promise.test.js
+++ __test__/promise.test.js
@@ -8,5 +8,13 @@ describe('Probando promesas', () => {
             expect(data.results.length).toBeGreaterThan(0);
             done();
         });
+
     });
+    test('Resuelve un hola', () => {
+        return expect(Promise.resolve('Hola!')).resolves.toBe('Hola!');
+    });
+
+    test('Rechaza con un error', () => {
+        return expect(Promise.reject('Error :(')).rejects.toBe('Error :(');
+    })
 })
